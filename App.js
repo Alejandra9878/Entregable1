6ef736d63c4e2717ba62b3d931740d592ce94029
Alejandra9878 <aleja1305cano@gmail.com>
2024-09-29 21:36:01 -0500

    Actualizacion

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import AppNavigator from './navigation/AppNavigator'; // Asegúrate de que esta ruta es correcta
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <AppNavigator />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
